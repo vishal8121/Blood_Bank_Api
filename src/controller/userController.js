097c20d3d5c56008ba75086f875084faddc49d1c
vishal8121 <vishalkumarwins@gmail.com>
2023-07-18 18:56:57 +0530

    add check for email it is exist in the database or not

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -7,26 +7,37 @@
      @Response : request status in json format with status code
      @Description : userRegister function request data from user. addUser function require from service file and called in this function and pass req.body data as arguments , In response user status return */
     exports.userRegister = async(req,res)=>{ 
-        const data = await service.addUser({
-            name: req.body.name,
-            email: req.body.email,
-            age: req.body.age, 
-            gender: req.body.gender,
-            blood_group: req.body.blood_group,
-            password: md5(req.body.password),  
-            role: "user",
-            phone_number: req.body.phone_number,
-            address: req.body.address,
-            last_donation_date: req.body.last_donation_date,
-            status: "inactive",
-            is_delete: false,
-            created_by: req.body.name
-          })
-        res.status(201).json({
-            status: "201",
-            data: data,
-            msg: "User created successfully"
-        });
-       
+            const user = await service.checkEmail(req.body.email);    
+            if(user == null){
+                const userData = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    age: req.body.age, 
+                    gender: req.body.gender,
+                    blood_group: req.body.blood_group,
+                    password: md5(req.body.password),  
+                    role: "user",
+                    phone_number: req.body.phone_number,
+                    address: req.body.address,
+                    last_donation_date: req.body.last_donation_date,
+                    status: "inactive",
+                    is_delete: false,
+                    created_by: req.body.name
+                }
+                const data = await service.addUser(userData);
+                return res.status(201).json({
+                    status: "201",
+                    data: data,
+                    msg: "User created successfully"
+                    
+                });      
+    }
+    else{
+        res.json({
+            status:"200",
+            msg: "User not created",
+            error: "user email already registered"
+        }); 
+    }
     }
     
