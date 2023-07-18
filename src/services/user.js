3738b9cb2775dedb0f5627dd38ba585d69126192
vishal8121 <vishalkumarwins@gmail.com>
2023-07-18 16:43:29 +0530

    add comment

--- src/services/user.js
+++ src/services/user.js
@@ -2,6 +2,9 @@ const db = require('../model')
 const User = db.user;
 const sequelize = db.sequelize;
 
+/*
+Description : created addUser function for create user and save user data into database. if user data save into database this function return user data, if return error throw error
+*/
 exports.addUser = async(userData)=>{
   try{
     await sequelize.sync();
