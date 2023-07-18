3bb76f9678566b97514232b5266b61ba86d6c672
vishal8121 <vishalkumarwins@gmail.com>
2023-07-18 17:28:34 +0530

    add index route file

--- src/route/user.js
+++ src/route/user.js
@@ -1,7 +1,4 @@
-const express = require('express');
-const router = express.Router();
 const userController = require("../controller/userController")
-const userMiddleware = require("../middleware/user");
 
-router.post('/', userMiddleware, userController.userRegister)
-module.exports = router
+exports.userRegisterRoute = userController.userRegister;
+
