51a564f7c0d75e7a8e2d437bb7bcc5fcf3a1eb95
vishal8121 <vishalkumarwins@gmail.com>
2023-07-18 17:13:52 +0530

    add comment for middleware function

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -5,6 +5,7 @@ const userMiddleware = require('../utils/user')
 @request : req.body data
 @response : status message.
 @Description: data function is middleware function for validation of user data.
+require userMiddleware from utils and pass user(req.body) as argument in userMiddleware function. 
 if error any error return then response error status message with mentioned error and if no error occur then next() return
 */
 const data =(req,res,next)=>{
