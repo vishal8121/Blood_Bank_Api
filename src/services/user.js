219ed8317dfd5018ff45a041df3f96432b80f73f
vishal8121 <vishalkumarwins@gmail.com>
2023-07-18 17:46:24 +0530

    add function for check existing email in database

--- src/services/user.js
+++ src/services/user.js
@@ -2,9 +2,11 @@ const db = require('../model')
 const User = db.user;
 const sequelize = db.sequelize;
 
-/*
-Description : created addUser function for create user and save user data into database. if user data save into database this function return user data, if return error throw error
-*/
+/****************************************************************************** 
+@params : userData
+@Description : created addUser function for create user and save user data into database. if user data save into database this function return user data, if return error throw error
+
+*******************************************************************************/
 exports.addUser = async(userData)=>{
   try{
     await sequelize.sync();
@@ -16,3 +18,23 @@ exports.addUser = async(userData)=>{
     console.log(e);
   }
 }
+
+
+/****************************************************************************** 
+@params : email
+@Description : created checkEmail check existing email if email exists in database it return that user and if error occurs then throw error
+
+*******************************************************************************/
+
+exports.checkEmail = async (email) => {
+  try {
+      const user = await User.findOne({
+          where: {
+              email: email
+          }
+      })
+      return user;
+  } catch (e) {
+      throw Error('Error:'+e)
+  }
+};
