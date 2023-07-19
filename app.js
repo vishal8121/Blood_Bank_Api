57cfc7ec7f8f526679542237187ecfc4300fd4b4
vishal8121 <vishalkumarwins@gmail.com>
2023-07-19 12:58:54 +0530

    replace port with env variable

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser')
 app.use(express.json());
 app.use(router); 
 app.use(bodyParser.json()) 
-  
+    
 app.listen(port, ()=>{   
     console.log(`Server running on port ${port}`);
 })                          
