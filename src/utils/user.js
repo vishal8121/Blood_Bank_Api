41d037f8deeb5597703d0027b0c0b794e6c76efc
vishal8121 <vishalkumarwins@gmail.com>
2023-07-19 10:51:53 +0530

    create validation for update data

--- src/utils/user.js
+++ src/utils/user.js
@@ -42,4 +42,36 @@ const User = (user)=>{
 return userSchema.validate(user)
 }
 
-module.exports = User;
+const updatedUser = (user)=>{
+        userSchema = Joi.object({
+           name: Joi.string()
+                 .min(3)
+                 .max(12),
+          
+             email: Joi.string()
+                    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+       
+             age: Joi.number(),
+       
+             gender: Joi.string(),
+       
+             blood_group: Joi.string(),
+       
+             password: Joi.string()
+                       .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+       
+             phone_number: Joi.string()
+                           .length(10)
+                           .pattern(/^[0-9]+$/),
+             address: Joi.string(),
+       
+             last_donation_date: Joi.date(),
+             updated_by: Joi.string()
+                         .required(),
+       
+       });
+       return userSchema.validate(user)
+       }
+
+
+module.exports = {User,updatedUser};
