aa22cbd3eca273834781e7ddf729f85120c44ac8
vishal8121 <vishalkumarwins@gmail.com>
2023-07-19 18:55:45 +0530

    add verifyToken middleware in put and delete route

--- src/route/index.js
+++ src/route/index.js
@@ -10,9 +10,12 @@ router.post('/', userMiddleware.data, userRoute.userRegisterRoute);
 
 router.get('/users',userRoute.allUsersRoute);
 
-router.put('/updateUser/:id',userMiddleware.updateData,userRoute.updateUserRoute);
+router.put('/updateUser/:id',userMiddleware.verifyToken,userMiddleware.updateData,userRoute.updateUserRoute);
 
-router.delete('/delete/:id',userRoute.deleteUserRoute);
-module.exports = router
+router.delete('/delete/:id',userMiddleware.verifyToken, userRoute.deleteUserRoute);
 
 router.patch('/login',userMiddleware.userLoginAuth, userRoute.loginUserRoute)
+
+
+module.exports = router
+
