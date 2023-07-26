34ccd9f1242c03c3c550f867238a72d2b1b85eb1
vishal8121 <vishalkumarwins@gmail.com>
2023-07-26 12:54:30 +0530

    add bloodbank model in index file

---
+++ src/models/bloodBank.js
@@ -0,0 +1,37 @@
+module.exports=(sequelize,DataTypes)=>{
+    const Blood_bank = sequelize.define('Blood_bank', {
+       name: {
+         type: DataTypes.STRING,
+         allowNull: false
+        
+       },
+       phone_number:{
+        type: DataTypes.STRING,
+        allowNull: false
+       },
+       address:{
+        type: DataTypes.STRING,
+        allowNull: false
+       },
+       status:{
+        type: DataTypes.STRING,
+        allowNull: false
+       },
+       account_status:{
+        type: DataTypes.STRING,
+        allowNull:false
+       },
+       created_by:{
+        type: DataTypes.STRING,
+        allowNull: false,
+       },
+       updated_by:{
+        type: DataTypes.STRING,
+       }
+    },  {
+      tableName: 'blood_banks',
+      paranoid: true
+    });
+        
+    return Blood_bank;
+    }
