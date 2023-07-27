b45d1dfac103a927988de3906c8603a26f4f006a
vishal8121 <vishalkumarwins@gmail.com>
2023-07-27 15:21:38 +0530

    create function for blood bank database query

---
+++ src/services/bloodBank.js
@@ -0,0 +1,81 @@
+const db = require('../models')
+const bloodBankAdmin = db.user;
+const bloodBank = db.bloodBank;
+
+/****************************************************************************** 
+@params : Data
+@Description : created addBloodBank function for create bloodbank and save blood bank data into database. if blood bank data save into database this function return blood bank data, if return error throw error
+
+*******************************************************************************/
+exports.addBloodBank = async(data,info)=>{
+  try{
+    await bloodBankAdmin.create(info)
+    const bloodB = await bloodBank.create(data)
+    return bloodB
+  }
+  catch(e){
+    console.log(e);
+  }
+}
+
+exports.checkEmail = async (email) => {
+  try {
+      const user = await bloodBankAdmin.findOne({
+          where: {
+              email: email
+          }
+      })
+      return user;
+  } catch (e) {
+      throw Error('Error:'+e)
+  }
+};
+
+exports.findId = async (id) => {
+  try {
+      const user = await bloodBankAdmin.findOne({ 
+          where: {
+              id: id
+          }
+      })
+      // console.log(user)
+      return user;
+  } catch (e) {
+      throw e ;
+  }
+};
+
+
+exports.getBloodBankReq = async()=>{
+  try{
+    const requestedBanks = await bloodBank.findAll({
+      where: {
+        status: "pending"
+      }
+    })
+    // console.log(requestedBanks)
+    return requestedBanks;
+    }
+  catch(e){
+    console.log(e);
+  }
+}
+
+
+exports.processRequest = async(reqStatus,name) =>{
+  try{
+     const request = await bloodBank.update({
+      status : reqStatus
+     },
+     {
+      where : {
+       name : name
+      }
+     }
+     )
+     return request
+  }
+  catch(e){
+    throw Error('Error :'+e);
+  }
+}
