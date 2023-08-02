8d2d8681707c57396ac26e1c9733ebc00c6c0769
vishal8121 <vishalkumarwins@gmail.com>
2023-08-02 11:50:32 +0530

    add enums

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -1,19 +1,22 @@
 const Enum = require('enum')
 
 const messages = new Enum({
+     'not_created': "Account not created",
      'registered': "Account created successfully",
      'not_registered': "Account not registered",
      'account_deleted': "Account deleted successfully",
      'password_incorrect': "Password incorrect",
      'login_success': "Login successfully",
      'permission_denied': "Permission denied",
-     'update_success': "Update successfully",
+     'update_success': "Updated successfully",
      'email_exists': "Email already exists",
      'data_not_found': "Data not found",
      'all_data': "All data",
      'under_process': "Your request are under processing",
      'login':"Please login",
-     'not_exist': "Not Exist"
+     'not_exist': "Not Exist",
+     'add_data_success': "Data added successfully",
+     'enter_data': "Please Enter details"
     })
 
 
