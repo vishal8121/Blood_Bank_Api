b922486c3b4c24e2f9874bf331b1908be0830d1b
vishal8121 <vishalkumarwins@gmail.com>
2023-08-06 20:03:00 +0530

    update

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.use(bodyParser.json())
       
 app.listen(port, ()=>{     
     console.log(`Server running on port ${port}`);
-})                             
+})                               
--- src/controller/bloodBankController.js
+++ src/controller/bloodBankController.js
@@ -269,7 +269,7 @@ exports.approveBloodRequest = async (req,res)=>{
     if(data.role == 'blood_bank' && data.status == 'active'){
         const approved = await service.acceptBloodRequest(req.body.id)
         if(approved){
-            console.log(approved)
+            // console.log(approved)  
             const bloodReq = await service.findBloodRequest(req.body.id)
             console.log(bloodReq.BloodBankId+"Vishal")
             const inventory = await service.findBloodInventory(bloodReq.BloodBankId)
