38724bdb86f041132acc9f4d1312970dfdda8a63
vishal8121 <vishalkumarwins@gmail.com>
2023-08-07 09:22:06 +0530

    improve exception handling

--- src/controller/bloodBankController.js
+++ src/controller/bloodBankController.js
@@ -21,49 +21,55 @@ const MESSAGE = require('../utils/enums');
  *****************************************************************************/
 
 exports.bloodBankRegister = async (req, res) => {
-    await sequelize.sync();
-    const bloodBank = await service.checkEmail(req.body.email);
-    if (bloodBank == null) {
-        console.log(bloodBank);
-        encryptedPassword = await bcrypt.hash(req.body.password, 10);
-        const info = {
-            name: req.body.name,
-            email: req.body.email,
-            password: encryptedPassword,
-            role: "blood_bank",
-            phone_number: req.body.phone_number,
-            address: req.body.address,
-            status: "inactive",
-            age: "24",
-            gender: "Male",
-            created_by: req.body.name
+    try{
+        await sequelize.sync();
+        const bloodBank = await service.checkEmail(req.body.email);
+        if (bloodBank == null) {
+            console.log(bloodBank);
+            encryptedPassword = await bcrypt.hash(req.body.password, 10);
+            const info = {
+                name: req.body.name,
+                email: req.body.email,
+                password: encryptedPassword,
+                role: "blood_bank",
+                phone_number: req.body.phone_number,
+                address: req.body.address,
+                status: "inactive",
+                age: "24",
+                gender: "Male",
+                created_by: req.body.name
+            }
+            await service.addBloodBankAdmin(info);
+            const getAdmin = await service.checkEmail(req.body.email);
+            const bloodBankDetails = {
+                name: req.body.name,
+                license_no: req.body.license_no,
+                address: req.body.address,
+                status: "pending",
+                created_by: req.body.name,
+                UserId: getAdmin.dataValues.id
+            }
+            const data = await service.addBloodBankDetails(bloodBankDetails)
+            // console.log(bloodBank)
+            if (data) {
+                let mailDetails = {
+                    from: 'vishalkumarwins@gmail.com',
+                    to: req.body.email,
+                    subject: 'Blood Bank Request',
+                    text: "Your registration has been received and is currently being processed by our team!"
+                };
+                getAllUtils.sendEmail(mailDetails)
+            }
+            return response(res,MESSAGE.under_process.value, data, "201")
         }
-        await service.addBloodBankAdmin(info);
-        const getAdmin = await service.checkEmail(req.body.email);
-        const bloodBankDetails = {
-            name: req.body.name,
-            license_no: req.body.license_no,
-            address: req.body.address,
-            status: "pending",
-            created_by: req.body.name,
-            UserId: getAdmin.dataValues.id
+        else {
+            return response(res,MESSAGE.not_created.value, null, 200, "Blood Bank already registered")
         }
-        const data = await service.addBloodBankDetails(bloodBankDetails)
-        // console.log(bloodBank)
-        if (data) {
-            let mailDetails = {
-                from: 'vishalkumarwins@gmail.com',
-                to: req.body.email,
-                subject: 'Blood Bank Request',
-                text: "Your registration has been received and is currently being processed by our team!"
-            };
-            getAllUtils.sendEmail(mailDetails)
-        }
-        return response(res,MESSAGE.under_process.value, data, "201")
     }
-    else {
-        return response(res,MESSAGE.not_created.value, null, 200, "Blood Bank already registered")
+    catch(err){
+        return response(res, "Internal Server Error" + e, null, "500", true);
     }
+   
 }
 
 
@@ -164,124 +170,147 @@ exports.processBankRequest = async (req, res) => {
 }
 
 exports.addBloodBankInventory = async(req, res)=>{
-    if(req.data){
-        const data = await service.findId(req.data);
-        const bloodBank = await service.checkBank(data.name)
-        const existInventory = await service.checkExistBankInventory(bloodBank.id)
-        const bloodUnits = {
-            a_positive: req.body.a_positive,
-            a_negative: req.body.a_negative,
-            b_positive: req.body.b_positive,
-            b_negative: req.body.b_negative,
-            ab_positive: req.body.ab_positive,
-            ab_negative: req.body.ab_negative,
-            o_positive: req.body.o_positive,
-            o_negative: req.body.o_negative,
-            BloodBankId: bloodBank.id,
-            created_by: data.name
-          }
-          const price = {
-            a_positive:req.body.a_positive_price,
-            a_negative:req.body.a_negative_price,
-            b_positive:req.body.b_positive_price,
-            b_negative:req.body.b_negative_price,
-            ab_positive:req.body.ab_positive_price,
-            ab_negative:req.body.ab_negative_price,
-            o_positive:req.body.o_positive_price,
-            o_negative:req.body.o_negative_price,
-            BloodBankId: bloodBank.id,
-            created_by: data.name
-          }
-        if(existInventory == null){
-              const bloodUnit = await service.addBloodUnits(bloodUnits); 
-              const unitPrice = await service.addBloodUnitPrice(price);
-              if(bloodUnit){
-                return response(res,MESSAGE.add_data_success.value, {Data:{bloodUnit:bloodUnit, bloodPrice: unitPrice}}, "201");
+    try{
+        if(req.data){
+            const data = await service.findId(req.data);
+            const bloodBank = await service.checkBank(data.name)
+            const existInventory = await service.checkExistBankInventory(bloodBank.id)
+            const bloodUnits = {
+                a_positive: req.body.a_positive,
+                a_negative: req.body.a_negative,
+                b_positive: req.body.b_positive,
+                b_negative: req.body.b_negative,
+                ab_positive: req.body.ab_positive,
+                ab_negative: req.body.ab_negative,
+                o_positive: req.body.o_positive,
+                o_negative: req.body.o_negative,
+                BloodBankId: bloodBank.id,
+                created_by: data.name
               }
-              else{
-                return response(res,MESSAGE.enter_data.value, null, "200",true);
+              const price = {
+                a_positive:req.body.a_positive_price,
+                a_negative:req.body.a_negative_price,
+                b_positive:req.body.b_positive_price,
+                b_negative:req.body.b_negative_price,
+                ab_positive:req.body.ab_positive_price,
+                ab_negative:req.body.ab_negative_price,
+                o_positive:req.body.o_positive_price,
+                o_negative:req.body.o_negative_price,
+                BloodBankId: bloodBank.id,
+                created_by: data.name
               }
+            if(existInventory == null){
+                  const bloodUnit = await service.addBloodUnits(bloodUnits); 
+                  const unitPrice = await service.addBloodUnitPrice(price);
+                  if(bloodUnit){
+                    return response(res,MESSAGE.add_data_success.value, {Data:{bloodUnit:bloodUnit, bloodPrice: unitPrice}}, "201");
+                  }
+                  else{
+                    return response(res,MESSAGE.enter_data.value, null, "200",true);
+                  }
+            }
+            return response(res,MESSAGE.all_data.value, existInventory, "200",true);
+        }else{
+            return response(res,MESSAGE.permission_denied.value + e, null, "500", true);
         }
-        return response(res,MESSAGE.all_data.value, existInventory, "200",true);
-    }else{
-        return response(res,MESSAGE.permission_denied.value + e, null, "500", true);
     }
+    catch(e){
+        return response(res, "Internal Server Error" + e, null, "500", true);
+    }
+  
 }
 
 
 exports.updateBloodBankInventory = async(req,res)=>{
-    if(req.data){
-        const data = await service.findId(req.data);
-        const bloodBank = await service.checkBank(data.name)
-        const existInventory = await service.checkExistBankInventory(bloodBank.id)
-        if(existInventory){
-        const bloodUnits = {
-            a_positive: req.body.a_positive,
-            a_negative: req.body.a_negative,
-            b_positive: req.body.b_positive,
-            b_negative: req.body.b_negative,
-            ab_positive: req.body.ab_positive,
-            ab_negative: req.body.ab_negative,
-            o_positive: req.body.o_positive,
-            o_negative: req.body.o_negative,
-            BloodBankId: bloodBank.id,
-            updated_by: data.name
-          }
-          const price = {
-            a_positive:req.body.a_positive_price,
-            a_negative:req.body.a_negative_price,
-            b_positive:req.body.b_positive_price,
-            b_negative:req.body.b_negative_price,
-            ab_positive:req.body.ab_positive_price,
-            ab_negative:req.body.ab_negative_price,
-            o_positive:req.body.o_positive_price,
-            o_negative:req.body.o_negative_price,
-            BloodBankId: bloodBank.id,
-            updated_by: data.name
-          }
-        const bloodUnit = await service.updateInventory(bloodBank.id,bloodUnits); 
-        const unitPrice = await service.updatePrice(bloodBank.id,price);
-        if(bloodUnit){
-            return response(res,MESSAGE.update_success.value, {
-             units:bloodUnits,
-             price: price    
-            }, "200");
+    try{
+        if(req.data){
+            const data = await service.findId(req.data);
+            const bloodBank = await service.checkBank(data.name)
+            const existInventory = await service.checkExistBankInventory(bloodBank.id)
+            if(existInventory){
+            const bloodUnits = {
+                a_positive: req.body.a_positive,
+                a_negative: req.body.a_negative,
+                b_positive: req.body.b_positive,
+                b_negative: req.body.b_negative,
+                ab_positive: req.body.ab_positive,
+                ab_negative: req.body.ab_negative,
+                o_positive: req.body.o_positive,
+                o_negative: req.body.o_negative,
+                BloodBankId: bloodBank.id,
+                updated_by: data.name
+              }
+              const price = {
+                a_positive:req.body.a_positive_price,
+                a_negative:req.body.a_negative_price,
+                b_positive:req.body.b_positive_price,
+                b_negative:req.body.b_negative_price,
+                ab_positive:req.body.ab_positive_price,
+                ab_negative:req.body.ab_negative_price,
+                o_positive:req.body.o_positive_price,
+                o_negative:req.body.o_negative_price,
+                BloodBankId: bloodBank.id,
+                updated_by: data.name
+              }
+            const bloodUnit = await service.updateInventory(bloodBank.id,bloodUnits); 
+            const unitPrice = await service.updatePrice(bloodBank.id,price);
+            if(bloodUnit){
+                return response(res,MESSAGE.update_success.value, {
+                 units:bloodUnits,
+                 price: price    
+                }, "200");
+            }
+            }
+            return response(res,MESSAGE.data_not_found.value, null, "200",true);
         }
-        }
-        return response(res,MESSAGE.data_not_found.value, null, "200",true);
     }
+    catch(e){
+        return response(res, "Internal Server Error" + e, null, "500", true);
+    }
+   
 }
 
 exports.getPendingBloodRequest = (async (req, res) => {
-    const data = await service.findId(req.data);
-    if(data.role == 'blood_bank' && data.status == 'active'){
-        const allRequest = await service.pendingBloodRequest()
-        if (allRequest.length == 0) {
-            return response(res,MESSAGE.data_not_found.value,null,"200");
+    try{
+        const data = await service.findId(req.data);
+        if(data.role == 'blood_bank' && data.status == 'active'){
+            const allRequest = await service.pendingBloodRequest()
+            if (allRequest.length == 0) {
+                return response(res,MESSAGE.data_not_found.value,null,"200");
+            }
+            return response(res,MESSAGE.all_data.value,allRequest,"200");
         }
-        return response(res,MESSAGE.all_data.value,allRequest,"200");
+        return response(res,MESSAGE.permission_denied.value, null, "403");
     }
-    return response(res,MESSAGE.permission_denied.value, null, "403");
+    catch(e){
+        return response(res, "Internal Server Error" + e, null, "500", true);
+    }
+  
 });
 
 exports.approveBloodRequest = async (req,res)=>{
-    const data = await service.findId(req.data);
-    if(data.role == 'blood_bank' && data.status == 'active'){
-        const approved = await service.acceptBloodRequest(req.body.id)
-        if(approved){
-            // console.log(approved)  
-            const bloodReq = await service.findBloodRequest(req.body.id)
-            // console.log(bloodReq.BloodBankId+"Vishal")
-            const inventory = await service.findBloodInventory(bloodReq.BloodBankId)
-            const decrement = inventory[bloodReq.blood_group] - bloodReq.units
-            console.log(inventory[bloodReq.blood_group] - bloodReq.units)
-            const data = {}
-            data[bloodReq.blood_group] = decrement
-            await service.bloodRequestIncrement(bloodReq.BloodBankId,data)
-        } 
-        return response(res,MESSAGE.request_approved.value,approved,"200");
+    try{
+        const data = await service.findId(req.data);
+        if(data.role == 'blood_bank' && data.status == 'active'){
+            const approved = await service.acceptBloodRequest(req.body.id)
+            if(approved){
+                // console.log(approved)  
+                const bloodReq = await service.findBloodRequest(req.body.id)
+                // console.log(bloodReq.BloodBankId+"Vishal")
+                const inventory = await service.findBloodInventory(bloodReq.BloodBankId)
+                const decrement = inventory[bloodReq.blood_group] - bloodReq.units
+                console.log(inventory[bloodReq.blood_group] - bloodReq.units)
+                const data = {}
+                data[bloodReq.blood_group] = decrement
+                await service.bloodRequestIncrement(bloodReq.BloodBankId,data)
+            } 
+            return response(res,MESSAGE.request_approved.value,approved,"200");
+        }
+        return response(res,MESSAGE.permission_denied.value, null, "403")
     }
-    return response(res,MESSAGE.permission_denied.value, null, "403")
+    catch(e){
+        return response(res, "Internal Server Error" + e, null, "500", true);
+    }
 }
 
 exports.collectBlood = async(req,res)=>{
@@ -306,6 +335,6 @@ exports.collectBlood = async(req,res)=>{
         return response(res,MESSAGE.permission_denied.value, null, "403")
     }
     catch(e){
-        throw e;
+        return response(res, "Internal Server Error" + e, null, "500", true);
     }
 }
